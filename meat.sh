af63e0abdf0e8b7d5bbf3a13103ed6d98c1a2b2a
Jakub Witkiewicz <krow10@gmail.com>
2014-08-21 00:29:00 +0100

    connect meat

--- meat.sh
+++ meat.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-CONN=`tmate show-messages | grep "Remote session:" | awk -F"ssh " '{print $2}'`
-HOST=`hostname`
-curl --data "host=$HOST&conn=$CONN" http://skymeat.herokuapp.com/save
+if [  -n "$1" ]; then
+   SSH_CONN=`curl -s http://skymeat.herokuapp.com/get/$1`
+   if [[ $SSH_CONN == ssh* ]]; then
+  	 `$SSH_CONN`
+  	fi 
+else
+   CONN=`tmate show-messages | grep "Remote session:" | awk -F"ssh " '{print $2}'`
+   HOST=`hostname`
+   curl --data "host=$HOST&conn=$CONN" http://skymeat.herokuapp.com/save
+   echo ""
+fi
+
+
+
